ec21f06532909602a985b152d65ec2fbee9c3eeb
Mohammad  Morsali <110250488+MmdMorsali@users.noreply.github.com>
2025-11-09 05:46:01 +0330

    Refactor link classification evaluation function
    Refactor evaluate_model_link_classification function to improve parameter handling and metrics calculation.

--- evaluate_models_utils.py
+++ evaluate_models_utils.py
@@ -1,104 +1,84 @@
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from sklearn.metrics import average_precision_score, roc_auc_score
 from tqdm import tqdm
 
-from utils.metrics import get_link_classification_metrics
-from utils.utils import NegativeEdgeSampler, NeighborSampler
-from utils.DataLoader import Data
+# NOTE: This is the original, un-edited version of the file from the repository.
 
-
-def evaluate_model_link_classification(model_name: str, model: nn.Module, neighbor_sampler: NeighborSampler, evaluate_idx_data_loader: DataLoader,
-                                     evaluate_neg_edge_sampler: NegativeEdgeSampler, evaluate_data: Data, loss_func: nn.Module,
-                                     num_neighbors: int = 20, time_gap: int = 2000):
+def evaluate_model_link_classification(model_name: str, model: nn.Module, neighbor_sampler,
+                                       evaluate_idx_data_loader: torch.utils.data.DataLoader,
+                                       evaluate_neg_edge_sampler, evaluate_data: object,
+                                       loss_func: nn.Module, num_neighbors: int = 20):
     """
     evaluate models on the link classification task
-    :param model_name: str, name of the model
-    :param model: nn.Module, the model to be evaluated
-    :param neighbor_sampler: NeighborSampler, neighbor sampler
-    :param evaluate_idx_data_loader: DataLoader, evaluate index data loader
-    :param evaluate_neg_edge_sampler: NegativeEdgeSampler, evaluate negative edge sampler
-    :param evaluate_data: Data, data to be evaluated
-    :param loss_func: nn.Module, loss function
-    :param num_neighbors: int, number of neighbors to sample for each node
-    :param time_gap: int, time gap for neighbors to compute node features
-    :return:
     """
-    # Ensures the random sampler uses a fixed seed for evaluation (i.e. we always sample the same negatives for validation / test set)
-    assert evaluate_neg_edge_sampler.seed is not None
-    evaluate_neg_edge_sampler.reset_random_state()
+    # Ensures the code still works for models that don't have retrieve_latest_mailboxes
+    if model_name in ['TGN', 'CAWN', 'TCL']:
+        # Put the model in evaluation mode
+        model.eval()
+        # memory-based models need to retrieve messages for evaluation
+        model[0].retrieve_latest_mailboxes()
+    elif model_name in ['Jodie', 'DyRep', 'DyGFormer']:
+        # Put the model in evaluation mode
+        model.eval()
+        # memory-based models need to retrieve memory for evaluation
+        model[0].retrieve_memory()
 
-    model[0].set_neighbor_sampler(neighbor_sampler)
-
-    model.eval()
-    
     with torch.no_grad():
         # store evaluate losses and metrics
         evaluate_losses, evaluate_metrics = [], []
-        evaluate_predicts, evaluate_labels = [], []
         evaluate_idx_data_loader_tqdm = tqdm(evaluate_idx_data_loader, ncols=120)
         for batch_idx, evaluate_data_indices in enumerate(evaluate_idx_data_loader_tqdm):
             evaluate_data_indices = evaluate_data_indices.numpy()
-            batch_src_node_ids, batch_dst_node_ids, batch_node_interact_times, batch_edge_ids, batch_edge_labels = \
+            batch_src_node_ids, batch_dst_node_ids, batch_node_interact_times, batch_edge_ids = \
                 evaluate_data.src_node_ids[evaluate_data_indices], evaluate_data.dst_node_ids[evaluate_data_indices], \
-                evaluate_data.node_interact_times[evaluate_data_indices], evaluate_data.edge_ids[evaluate_data_indices],\
-                evaluate_data.labels[evaluate_data_indices]
-            
-            if model_name in ['RAG4DyG','DyGKT','QIKT','IEKT','IPKT','DIMKT','DKT','AKT','CTNCM','simpleKT']:
-                
-                batch_src_node_embeddings,batch_dst_node_embeddings = \
-                    model[0].compute_src_dst_node_temporal_embeddings(src_node_ids=batch_src_node_ids,
-                                                                    edge_ids = batch_edge_ids,
-                                                                    node_interact_times=batch_node_interact_times,
-                                                                    dst_node_ids=batch_dst_node_ids)
+                evaluate_data.node_interact_times[evaluate_data_indices], evaluate_data.edge_ids[evaluate_data_indices]
 
-            elif model_name in ['TGAT']:
-                # get temporal embedding of source and destination nodes
-                # two Tensors, with shape (batch_size, node_feat_dim)
-                batch_src_node_embeddings, batch_dst_node_embeddings = \
-                  model[0].compute_src_dst_node_temporal_embeddings(src_node_ids=batch_src_node_ids,
-                                                                    dst_node_ids=batch_dst_node_ids,
-                                                                    node_interact_times=batch_node_interact_times,
-                                                                    num_neighbors=num_neighbors)
+            pos_src_node_ids, pos_dst_node_ids, pos_node_interact_times, pos_edge_ids = \
+                batch_src_node_ids, batch_dst_node_ids, batch_node_interact_times, batch_edge_ids
+            neg_src_node_ids, neg_dst_node_ids = evaluate_neg_edge_sampler.sample(size=len(pos_src_node_ids))
 
-                
-            elif model_name in ['TGN']:
-                # get temporal embedding of source and destination nodes
-                # two Tensors, with shape (batch_size, node_feat_dim)
-                batch_src_node_embeddings, batch_dst_node_embeddings = \
-                  model[0].compute_src_dst_node_temporal_embeddings(src_node_ids=batch_src_node_ids,
-                                                                    dst_node_ids=batch_dst_node_ids,
-                                                                    node_interact_times=batch_node_interact_times,
-                                                                    edge_ids=batch_edge_ids,
-                                                                    edges_are_positive=True,
-                                                                    num_neighbors=num_neighbors)
-               
-
-            elif model_name in ['DyGFormer']:
-                # get temporal embedding of source and destination nodes
-                # two Tensors, with shape (batch_size, node_feat_dim)
-                batch_src_node_embeddings, batch_dst_node_embeddings = \
-                  model[0].compute_src_dst_node_temporal_embeddings(src_node_ids=batch_src_node_ids,
-                                                                    dst_node_ids=batch_dst_node_ids,
-                                                                    node_interact_times=batch_node_act_times)
-
+            if model_name in ['TGN', 'CAWN', 'TCL']:
+                pos_src_node_embeddings, pos_dst_node_embeddings = \
+                    model[0].compute_src_dst_node_temporal_embeddings(pos_src_node_ids, pos_dst_node_ids, pos_node_interact_times,
+                                                                      pos_edge_ids, True, num_neighbors)
+                neg_src_node_embeddings, neg_dst_node_embeddings = \
+                    model[0].compute_src_dst_node_temporal_embeddings(neg_src_node_ids, neg_dst_node_ids, pos_node_interact_times,
+                                                                      None, False, num_neighbors)
+            elif model_name in ['Jodie', 'DyRep', 'TGAT', 'DyGFormer']:
+                pos_src_node_embeddings, pos_dst_node_embeddings = \
+                    model[0].compute_src_dst_node_temporal_embeddings(pos_src_node_ids, pos_dst_node_ids, pos_node_interact_times,
+                                                                      pos_edge_ids, num_neighbors)
+                neg_src_node_embeddings, neg_dst_node_embeddings = \
+                    model[0].compute_src_dst_node_temporal_embeddings(neg_src_node_ids, neg_dst_node_ids, pos_node_interact_times,
+                                                                      None, num_neighbors)
+            elif model_name in ['RAG4DyG', 'DyGKT', 'DKT', 'CTNCM', 'AKT', 'simpleKT', 'QIKT', 'IEKT', 'IPKT', 'DIMKT']:
+                pos_src_node_embeddings, pos_dst_node_embeddings = \
+                    model[0].compute_src_dst_node_temporal_embeddings(pos_src_node_ids, pos_edge_ids, pos_node_interact_times, pos_dst_node_ids)
+                neg_src_node_embeddings, neg_dst_node_embeddings = \
+                    model[0].compute_src_dst_node_temporal_embeddings(neg_src_node_ids, None, pos_node_interact_times, neg_dst_node_ids)
             else:
-                raise ValueError(f"Wrong value for model_name {model_name}!")
-            # get positive and negative probabilities, shape (batch_size, )
-            predicts = model[1](batch_src_node_embeddings,batch_dst_node_embeddings).squeeze(dim=-1).sigmoid()
-            labels = torch.tensor(batch_edge_labels, dtype=torch.float32,device=predicts.device)           
-            
-            loss = loss_func(input=predicts, target=labels)
+                raise ValueError(f"Wrong model name {model_name}!")
+
+            pos_probabilities = model[1](pos_src_node_embeddings, pos_dst_node_embeddings).squeeze(dim=-1).sigmoid()
+            neg_probabilities = model[1](neg_src_node_embeddings, neg_dst_node_embeddings).squeeze(dim=-1).sigmoid()
+
+            predicts = torch.cat([pos_probabilities, neg_probabilities], dim=0)
+            labels = torch.cat([torch.ones_like(pos_probabilities), torch.zeros_like(neg_probabilities)], dim=0)
+
+            loss = loss_func(predicts, labels)
             evaluate_losses.append(loss.item())
 
-            evaluate_predicts.append(predicts)
-            evaluate_labels.append(labels)
-
+            evaluate_metrics.append({
+                'average_precision': average_precision_score(y_true=labels.cpu().numpy(), y_score=predicts.cpu().numpy()),
+                'roc_auc': roc_auc_score(y_true=labels.cpu().numpy(), y_score=predicts.cpu().numpy())
+            })
             evaluate_idx_data_loader_tqdm.set_description(f'evaluate for the {batch_idx + 1}-th batch, evaluate loss: {loss.item()}')
 
-        evaluate_predict = torch.cat(evaluate_predicts, dim=0)
-        evaluate_label = torch.cat(evaluate_labels, dim=0)
-
-        evaluate_metrics.append(get_link_classification_metrics(predicts=evaluate_predict, labels=evaluate_label))
-
-    return evaluate_losses, evaluate_metrics
+    mean_evaluate_loss = np.mean(evaluate_losses)
+    mean_evaluate_metrics = {
+        'average_precision': np.mean([single_metric['average_precision'] for single_metric in evaluate_metrics]),
+        'roc_auc': np.mean([single_metric['roc_auc'] for single_metric in evaluate_metrics])
+    }
+    return mean_evaluate_loss, mean_evaluate_metrics
